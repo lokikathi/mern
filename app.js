a8556241c13e9bf772e12045896f9b78625ecd06
Lokesh karthikeyan <12e8lokeshkarthikeyan@gmail.com>
2024-08-02 12:35:03 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,43 @@
+console.log("Lokesh")
+var num=5
+var num2=10
+var num3=0
+console.log(num+num2)
+console.log(num%num3)
+console.log(typeof(num3))
+str="ve"
+var n=1
+console.log(n+str)
+if(num>num2)
+{
+    console.log(num2)
+}
+else{
+    console.log(num)
+}
+let a=90;
+if(a==100){
+    console.log("Grade A")
+}
+else if(a>80){
+    console.log("Grade B")
+}
+else if(a>50){
+    console.log("Grade C")
+}
+else{
+    console.log("Fail")
+}
+for(let i=0;i<=3;i++){
+    console.log(i)
+}
+let k=0
+while(k<=3){
+    console.log(k)
+    k++
+}
+do{
+    g=0
+    console.log(g)
+}while(g==1)
+
